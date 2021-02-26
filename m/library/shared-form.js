4637372486d5d5636e23a41ab20b7ebc3243e8c0
Gunnar Unger <gunnar.unger@gmail.com>
2021-02-26 15:29:40 +1100

    update

--- m/library/shared-form.js
+++ m/library/shared-form.js
@@ -32,6 +32,7 @@ var I=0; var loop_1=setInterval(function (){
 var load=m.load;
 m.load=function(){
     load();
+    $('#title__ID').text($vm.module_list[$vm.vm['__ID'].name].task_name);
     if($vm.online_questionnaire==1) {
         $('#pdf__ID').hide();
         $('#participant_div__ID').hide();
